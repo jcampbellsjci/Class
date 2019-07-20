cfec30c4404f88d1af8594534ad1d76f1c0e23fb
John Campbell <jcampbellsjci@gmail.com>
2019-07-20 14:35:30 -0400

    Added week 1 materials

---
+++ Week 1/Materials/week 1.R
@@ -0,0 +1,12 @@
+#### Bare Bones R ####
+
+# R can be used as a calculator
+1 + 1
+25 / 5
+4 * 3
+
+# To save what we've done to an object, we use <- 
+test_output <- 1 + 1
+# We can then call this output later 
+test_output
+test_output + 3

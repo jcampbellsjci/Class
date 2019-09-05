2bc25840f37fc59b0044100f0009fd6f0c7e0d5c
John Campbell <jcampbellsjci@gmail.com>
2019-09-05 19:41:37 -0400

    Fixed Week 1

--- Week 1/Materials/week 1.R
+++ Week 1/Materials/week 1.R
@@ -1,3 +1,9 @@
+###########################################
+####     R Statistical Programming     ####
+####    Lesson 1: Workflow Overview    ####
+###########################################
+
+
 #### Bare Bones R ####
 
 # R can be used as a calculator
@@ -5,8 +11,21 @@
 25 / 5
 4 * 3
 
+2-2
+
+#### Assigning to Objects ####
+
 # To save what we've done to an object, we use <- 
 test_output <- 1 + 1
 # We can then call this output later 
 test_output
 test_output + 3
+
+
+#### Functions ####
+
+# Look up the absolute value of -1
+abs(x = -1)
+
+# Look up the help page
+?abs
